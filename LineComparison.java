48f1aa280cef39bf77ccbc1e2170e80c4cc15114
ctshara14 <ctsharath14@gmail.com>
2022-07-06 15:17:54 +0530

    Adding: Line equality Program

--- LineComparison.java
+++ LineComparison.java
@@ -1,30 +1,41 @@
-//Model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
+//check equality of two lines based on the end points, So that I know when two lines are the equal.
 
 import java.util.Scanner;
 
 public class LineComparison {
-	public static void main(String[] args) {
-	System.out.println("Welcome to Line Comparison Computation Program");
-	Scanner scan = new Scanner(System.in);
-	double x1 = 0;	
-	double y1 = 0;
-	double x2 = 0;
-	double y2 = 0;
-	double length = 0;
-	
-	System.out.println("Please enter X1 Coordinate");
-	x1 = scan.nextInt();
-	System.out.println("Please enter Y1 Coordinate");
-	y1 = scan.nextInt();
-	System.out.println("Please enter X2 Coordinate");
-	x2 = scan.nextInt();
-	System.out.println("Please enter Y2 Coordinate");
-	y2 = scan.nextInt();
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Please enter X1 Coordinate of line 1");
+        double x1 = Scan.nextDouble();
+	System.out.println("Please enter Y1 Coordinate of line 1");
+        double y1 = Scan.nextDouble();
+	System.out.println("Please enter X2 Coordinate of line 1");
+        double x2 = Scan.nextDouble();
+	System.out.println("Please enter Y2 Coordinate of line 1");
+        double y2 = Scan.nextDouble();
 
-	scan.close();
-
-	length = (Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))));
-	System.out.println("Length between 2 points is : "+length);
-	}
-} 
+	System.out.println("Please enter X1 Coordinate of line 2");
+        double a1 = Scan.nextDouble();
+	System.out.println("Please enter Y1 Coordinate of line 2");
+        double b1 = Scan.nextDouble();
+	System.out.println("Please enter X2 Coordinate of line 2");
+        double a2 = Scan.nextDouble();
+	System.out.println("Please enter Y2 Coordinate of line 2");
+        double b2 = Scan.nextDouble();
 	
+	Scan.close();
+
+        double length1 = Math.sqrt(((x2 - x1)*(x2 - x1) +(y2 - y1) * (y2 - y1)));
+        System.out.println("The length of line 1 is :" + length1);
+
+        double length2 = Math.sqrt(((a2 - a1)*(a2 - a1) +(b2 - b1) * (b2 - b1)));
+        System.out.println("The length of line 2 is :" + length2);
+
+        if (length1 == length2){
+            System.out.println(" Both the lines are equal");
+        } 
+	else {
+            System.out.println("Both the lines are not equal");
+        }
+    }
+}
