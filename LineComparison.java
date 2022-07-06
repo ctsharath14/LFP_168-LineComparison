730057b1d52cea9e274a0514e784236f3e5b1106
ctshara14 <ctsharath14@gmail.com>
2022-07-06 12:57:46 +0530

    Adding: Distance between line

--- LineComparison.java
+++ LineComparison.java
@@ -1,12 +1,29 @@
 //Model a line based on a point consisting of (x, y) co-ordinates using the Cartesian system, So that I can calculate its length.
 
+import java.util.Scanner;
+
 public class LineComparison {
 	public static void main(String[] args) {
-	int x1 = 4;	
-	int y1 = 4;
-	int x2 = 10;
-	int y2 = 10;
-	int length = (int) Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1)));
+	System.out.println("Welcome to Line Comparison Computation Program");
+	Scanner scan = new Scanner(System.in);
+	double x1 = 0;	
+	double y1 = 0;
+	double x2 = 0;
+	double y2 = 0;
+	double length = 0;
+	
+	System.out.println("Please enter X1 Coordinate");
+	x1 = scan.nextInt();
+	System.out.println("Please enter Y1 Coordinate");
+	y1 = scan.nextInt();
+	System.out.println("Please enter X2 Coordinate");
+	x2 = scan.nextInt();
+	System.out.println("Please enter Y2 Coordinate");
+	y2 = scan.nextInt();
+
+	scan.close();
+
+	length = (Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))));
 	System.out.println("Length between 2 points is : "+length);
 	}
 } 
